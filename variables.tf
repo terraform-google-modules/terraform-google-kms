095dc2500b5e180cb0665861ddb96e3a85208027
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-05-20 23:01:39 -0300

    Adds two news variable to hold version_template configurations

--- variables.tf
+++ variables.tf
@@ -82,3 +82,14 @@ variable "key_rotation_period" {
   default = "100000s"
 }
 
+variable "key_algorithm" {
+  type        = string
+  description = "The algorithm to use when creating a version based on this template. See the https://cloud.google.com/kms/docs/reference/rest/v1/CryptoKeyVersionAlgorithm for possible inputs."
+  default     = "GOOGLE_SYMMETRIC_ENCRYPTION"
+}
+
+variable "key_protection_level" {
+  type        = string
+  description = "The protection level to use when creating a version based on this template. Default value: \"SOFTWARE\" Possible values: [\"SOFTWARE\", \"HSM\"]"
+  default     = "SOFTWARE"
+}
