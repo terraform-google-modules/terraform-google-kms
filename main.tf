4af65d82c59c5edb7f7ea70a657f985ab2f74c20
kpedersen <kpedersen@pythian.com>
2021-07-09 14:12:11 -0400

    removing string split on element index

--- main.tf
+++ main.tf
@@ -64,20 +64,20 @@ resource "google_kms_crypto_key_iam_binding" "owners" {
   count         = length(var.set_owners_for)
   role          = "roles/owner"
   crypto_key_id = local.keys_by_name[var.set_owners_for[count.index]]
-  members       = compact(split(",", var.owners[count.index]))
+  members       = compact(var.owners)
 }
 
 resource "google_kms_crypto_key_iam_binding" "decrypters" {
   count         = length(var.set_decrypters_for)
   role          = "roles/cloudkms.cryptoKeyDecrypter"
   crypto_key_id = local.keys_by_name[var.set_decrypters_for[count.index]]
-  members       = compact(split(",", var.decrypters[count.index]))
+  members       = compact(var.decrypters)
 }
 
 resource "google_kms_crypto_key_iam_binding" "encrypters" {
   count         = length(var.set_encrypters_for)
   role          = "roles/cloudkms.cryptoKeyEncrypter"
   crypto_key_id = local.keys_by_name[element(var.set_encrypters_for, count.index)]
-  members       = compact(split(",", var.encrypters[count.index]))
+  members       = compact(var.encrypters)
 }
 
