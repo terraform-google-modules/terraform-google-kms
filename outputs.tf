e4c39a5a12c6d1581bfec57cc927782823b7fa40
Luiz Dutra <luizdutra@ciandt.com>
2021-08-10 17:28:41 -0300

    added a dependencia between the kims and the grant access

--- outputs.tf
+++ outputs.tf
@@ -17,20 +17,44 @@
 output "keyring" {
   description = "Self link of the keyring."
   value       = google_kms_key_ring.key_ring.self_link
+  depends_on = [
+    # The grants are important to the key be ready to use.
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_resource" {
   description = "Keyring resource."
   value       = google_kms_key_ring.key_ring
+  depends_on = [
+    # The grants are important to the key be ready to use.
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keys" {
   description = "Map of key name => key self link."
   value       = local.keys_by_name
+  depends_on = [
+    # The grants are important to the key be ready to use.
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_name" {
   description = "Name of the keyring."
   value       = google_kms_key_ring.key_ring.name
+  depends_on = [
+    # The grants are important to the key be ready to use.
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
