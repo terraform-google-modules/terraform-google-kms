9f4129d87799c1c22c388e1a3808717b09a58ab3
LuizSDCit <86307064+LuizSDCit@users.noreply.github.com>
2021-08-11 15:54:05 -0300

    fix: Create dependency with grant (#46)
    * added a dependencia between the kims and the grant access
    
    * moved the comment

--- outputs.tf
+++ outputs.tf
@@ -17,20 +17,48 @@
 output "keyring" {
   description = "Self link of the keyring."
   value       = google_kms_key_ring.key_ring.self_link
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_resource" {
   description = "Keyring resource."
   value       = google_kms_key_ring.key_ring
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keys" {
   description = "Map of key name => key self link."
   value       = local.keys_by_name
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_name" {
   description = "Name of the keyring."
   value       = google_kms_key_ring.key_ring.name
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
