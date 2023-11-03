4ebd71f5866ba0b73fa9df36f8cabd964fb11b1e
Chris Wise <chris.wise@cyberhaven.com>
2023-11-03 17:08:49 +0000

    change key_destroy_scheduled_duration default to null

--- variables.tf
+++ variables.tf
@@ -43,9 +43,9 @@ variable "prevent_destroy" {
 }
 
 variable "key_destroy_scheduled_duration" {
-  description = "Set The period of time that versions of keys spend in the DESTROY_SCHEDULED state before transitioning to DESTROYED."
+  description = "Set the period of time that versions of keys spend in the DESTROY_SCHEDULED state before transitioning to DESTROYED."
   type        = string
-  default     = "86400s"
+  default     = null
 }
 
 variable "purpose" {
