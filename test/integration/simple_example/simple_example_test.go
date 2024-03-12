9343b4624c075113d12734c21518a2f37eaf01e9
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-03-12 11:23:21 -0300

    Add a filter param

--- test/integration/simple_example/simple_example_test.go
+++ test/integration/simple_example/simple_example_test.go
@@ -33,12 +33,13 @@ func TestSimpleExample(t *testing.T) {
 		location := bpt.GetStringOutput("location")
 		keys := [2]string{"one", "two"}
 
-		op := gcloud.Runf(t, "--project=%s kms keyrings list --location %s --sort-by ~createTime", projectId, location).Array()[0].Get("name")
+		op := gcloud.Runf(t, "--project=%s kms keyrings list --location %s --filter name:%s", projectId, location, keyring).Array()[0].Get("name")
 		assert.Contains(op.String(), fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", projectId, location, keyring), "Contains KeyRing")
 
 		op1 := gcloud.Runf(t, "kms keys list --project=%s --keyring %s --location %s", projectId, keyring, location).Array()
 		for index, element := range op1 {
 			assert.Equal(element.Get("primary").Map()["name"].Str, fmt.Sprintf("projects/%s/locations/%s/keyRings/%s/cryptoKeys/%s/cryptoKeyVersions/1", projectId, location, keyring, keys[index]), "Contains Keys")
+			assert.False(element.Get("importOnly").Bool(), "ImportOnly flag")
 		}
 	})
 
