090cf4a8f095744b3d8a910f7c400ecba1f77024
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-11-07 14:25:03 -0300

    Alert implemented on examples

---
+++ examples/monitoring_alerts/main.tf
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# Send a warning email when a KMS key version is scheduled for destruction.
+
+ module "kms" {
+  source  = "terraform-google-modules/kms/google"
+  version = "~> 3.2"
+
+  project_id = var.project_id
+  keyring    = "alert-keyring"
+  location   = "us-central1"
+  keys       = ["alert-key"]
+  prevent_destroy = false
+}
+
+resource "google_monitoring_alert_policy" "main" {
+  project      = var.project_id
+  display_name = "KMS Key Version Destruction Alert"
+  documentation {
+    content = "KMS Key Version alert: one or more key versions from ${var.project_id} project were scheduled for destruction."
+  }
+  combiner = "OR"
+  conditions {
+    display_name = "Destroy condition"
+    condition_matched_log {
+      filter = join(" AND ", flatten([
+        "protoPayload.request.@type=\"type.googleapis.com/google.cloud.kms.v1.DestroyCryptoKeyVersionRequest\"",
+        var.monitor_all_keys_in_the_project ? [] : ["protoPayload.request.name=~\"${values(module.kms.keys)[0]}/.*\""]
+      ]))
+    }
+  }
+
+  alert_strategy {
+    notification_rate_limit {
+      period = "300s"
+    }
+  }
+
+  notification_channels = [for email_ch in google_monitoring_notification_channel.email_channel : email_ch.name]
+
+  severity = "WARNING"
+}
+
+resource "google_monitoring_notification_channel" "email_channel" {
+  for_each     = toset(var.email_addresses_to_be_notified)
+
+  project      = var.project_id
+  display_name = "KMS version scheduled for destruction alert channel"
+  type         = "email"
+  description  = "Sends email notifications for KMS key versions scheduled for destruction alerts"
+
+  labels = {
+    email_address = each.value
+  }
+}
---
+++ examples/monitoring_alerts/variables.tf
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
+
+variable "monitor_all_keys_in_the_project" {
+  type        = bool
+  description = "True for all KMS key versions under the same project to be monitored, false for only the KMS key version created in this example to be monitored. Default: false."
+  default     = false
+}
+
+variable "email_addresses_to_be_notified" {
+  type        = list(string)
+  description = "Email addresses used for sending notifications to."
+}
